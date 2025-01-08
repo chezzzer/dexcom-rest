ce5c7f47e8a9b52d585aef60639a6896b2be4bda
ryan.cherry <ryan.cherry@rocketspark.com>
2025-01-08 14:25:41 +1300

    listen on 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ fastify.get('/', async (req, res) => {
 })
 
 try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ host: "0.0.0.0", port: 3000 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
