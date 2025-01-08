8339b26eb154024f881fd3a75c4a14e53a30e693
Chezzer <46093784+chezzzer@users.noreply.github.com>
2025-01-08 18:55:05 +1300

    add cors

--- src/inc/Fastify.ts
+++ src/inc/Fastify.ts
@@ -4,6 +4,4 @@ import cors from "@fastify/cors";
 export const fastify = Fastify({
     logger: true,
 });
-await fastify.register(cors, {
-    origin: ["chezzer.dev", "localhost:3000"],
-});
+await fastify.register(cors);
